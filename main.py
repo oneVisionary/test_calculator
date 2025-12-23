4e2dd6f31f38da3b3b600fab5e98f51a679d87d3
Fathima <142009811+oneVisionary@users.noreply.github.com>
2025-12-23 13:19:34 +0530

    Add simple calculator implementation in Python

---
+++ main.py
@@ -0,0 +1,20 @@
+# Simple calculator in Python
+
+a = 10
+b = 5
+
+# Addition
+add = a + b
+print("Addition:", add)
+
+# Subtraction
+subtract = a - b
+print("Subtraction:", subtract)
+
+# Multiplication
+multiply = a * b
+print("Multiplication:", multiply)
+
+# Division
+divide = a / b
+print("Division:", divide)
